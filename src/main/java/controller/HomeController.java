2fbaf793a468bf2174c626148d2529e79bb94528
froApril <188494506@qq.com>
2019-09-05 19:16:54 +1000

    Develop: login/signup css and js

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -2,8 +2,6 @@ package controller;
 
 import java.util.Locale;
 
-import dao.impl.UserEntityDaoImpl;
-import entities.UserEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -24,45 +22,14 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-        UserEntityDaoImpl user = new UserEntityDaoImpl();
 
-//        UserEntity test = new UserEntity();
-//	    test.setUsername("1278007");
-//	    test.setEmail("1");
-//	    test.setPassword("asd");
-//	    test.setNickname("asd");
-//	    test.setGender("asd");
-//	    test.setiSstaff(1);
-//	    test.setPerDes("sad");
-//	    test.setPhotoPro("asd");
-//	    test.setContactIno("asd");
-//	    user.addUser(test);
-//        user.setNewnickname(test,"rrrrrrrrrrrrrrrrr");
-
-//        UserEntity test1 = new UserEntity();
-//	    test.setUsername("111");
-//	    test.setEmail("123");
-//	    test.setPassword("1231");
-//	    test.setNickname("aaaaa");
-//	    test.setGender("ddddd");
-//	    test.setiSstaff(0);
-//	    test.setPerDes("sssss");
-//	    test.setPhotoPro("ssss");
-//	    test.setContactIno("asaad");
-//
-//	    user.addUser(test1);
-
-
-
-        UserEntity userEntity = user.getUserByName("xyf");
-        if(userEntity==null){
-            model.addAttribute("user", "null");
-        }
-        else{
-            model.addAttribute("user", userEntity.getUsername());
-        }
-
-		return "home";
+		return "login";
 	}
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String home1(Locale locale, Model model) {
+
+        return "login";
+    }
 	
 }
